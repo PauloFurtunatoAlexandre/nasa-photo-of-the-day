082d9718b7d478dca8f0aea24e472cd65c81391a
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-05-26 18:23:58 -0500

    added props on parent, passing to child NasaPost

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import NasaPost from "./NasaPost";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
+  const [apod, setApod] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=CtUPAONsTf6UnI5ZLRbJkgTogRdByfCgR6W6TGf2#")
+    .then(response => {
+      setApod(response.data);
+      console.log(response);
+    })
+    .catch(error => console.log("Something went wrong: ", error));
+  }, []);
+
+  return <NasaPost apod={apod} />;
+  //   <div className="App">
+      
+  //   </div>
+  // );
 }
 
 export default App;
