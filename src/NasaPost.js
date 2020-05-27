8e1fbac9f8f5f43639a200d8c241e816813922ee
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-05-26 20:26:08 -0500

    added the loading functionality

--- src/NasaPost.js
+++ src/NasaPost.js
@@ -3,26 +3,26 @@ import React from "react";
 const NasaPost = (props) => {
     const { apods } = props;
 
-    // if (!props.photoOfTheDay) {
-    //     return <h3>Loading...</h3>;
-    // }
-
-    return(
-        <div className="main--container">
-            <section className="title--section">
-            <h2 className="nasa--title">{apods.title}</h2>
-            </section>
-            
-            <div className="column--half">
-                <img className="image-of-the-day" src={apods.url} alt="Nasa image of the day" />
-                <span className="optional-info">{apods.copyright}</span>
+    if (!apods.url) {
+        return <h3 className="loading">Loading...</h3>;
+    } else {
+        return(
+            <div className="main--container">
+                <section className="title--section">
+                <h2 className="nasa--title">{apods.title}</h2>
+                </section>
+                
+                <div className="column--half">
+                    <img className="image-of-the-day" src={apods.url} alt="Nasa image of the day" />
+                    <span className="optional-info">{apods.copyright}</span>
+                </div>
+                <div className="column--half">
+                    <p>{apods.explanation}</p>
+                    <span className="optional-info">{apods.date}</span>
+                </div>
             </div>
-            <div className="column--half">
-                <p>{apods.explanation}</p>
-                <span>{apods.date}</span>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default NasaPost;
